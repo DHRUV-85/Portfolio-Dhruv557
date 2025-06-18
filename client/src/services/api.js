c061902bd990203965748dc62c651e79dd7f8aa3
DHRUV-85 <121780889+DHRUV-85@users.noreply.github.com>
2025-06-19 03:01:33 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "https://portfolio-dhruv557.onrender.com",
+  baseURL: "https://portfolio-dhruv557.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
